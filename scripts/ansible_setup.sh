cecc8cf18b8dccacaa4e32ee3845592258b32708
Riyaz Mohammed Ibrahim <riyaz@users.noreply.github.com>
2022-03-14 17:50:19 +0530

    fix: use brew to install ansible

--- scripts/ansible_setup.sh
+++ scripts/ansible_setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 OS_VERSION=$(sw_vers -productVersion | cut -d'.' -f 1-2)
 
@@ -8,10 +9,23 @@ PROD=$(softwareupdate -l |
   head -n 1 | awk -F"*" '{print $2}' |
   sed -e 's/^ *//' |
   tr -d '\n')
+
+echo "Update OS"
 softwareupdate -i "$PROD" --verbose
 rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
+echo "Update xcode"
+xcode-select —-install
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --user
-python -m pip install --user ansible
+echo "Install brew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew install ansible
+
+echo "Set a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 0.02
+
+echo "Set a shorter Delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+
